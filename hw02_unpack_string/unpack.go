0d913e5ae69dde2173af29360481c0cb668d74a6
Aleksei Verkhovtsev <seftomsk@gmail.com>
2021-06-28 21:10:51 +0600

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,51 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var ErrStartingAtDigit = errors.New("you pass string starting at digit")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+var ErrContainsNumber = errors.New("your string contains a number")
+
+func Unpack(rawStr string) (string, error) {
+	var strBuilder strings.Builder
+	rawStr = strings.TrimSpace(rawStr)
+
+	if len(rawStr) == 0 {
+		return "", nil
+	}
+
+	for idx, r := range rawStr {
+		if unicode.IsDigit(r) {
+			prevIdx := idx - 1
+			if prevIdx < 0 {
+				return "", ErrStartingAtDigit
+			}
+			prevRune := rune(rawStr[prevIdx])
+			if unicode.IsDigit(prevRune) {
+				return "", ErrContainsNumber
+			}
+			continue
+		}
+
+		nextIdx := idx + 1
+		if nextIdx < len(rawStr) {
+			nextRune := rune(rawStr[nextIdx])
+			if unicode.IsDigit(nextRune) {
+				countLetters := int(nextRune - '0')
+				currentLetter := string(rawStr[idx])
+				repeatedLetters := strings.Repeat(currentLetter, countLetters)
+				strBuilder.WriteString(repeatedLetters)
+				continue
+			}
+			strBuilder.WriteString(string(rawStr[idx]))
+			continue
+		}
+
+		strBuilder.WriteString(string(rawStr[idx]))
+	}
+
+	return strBuilder.String(), nil
 }
