b0ae9acedab37f6e8fcc2826ede4ebd19c04cbd3
Aleksei Verkhovtsev <seft@192.168.0.6>
2021-10-08 22:52:26 +0600

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,44 @@
 package main
 
+import (
+	"errors"
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	var err error
+	for key, value := range env {
+		if value.NeedRemove {
+			err = os.Unsetenv(key)
+			if err != nil {
+				log.Println(err)
+				return 1
+			}
+		}
+		err = os.Setenv(key, value.Value)
+		if err != nil {
+			log.Println(err)
+			return 1
+		}
+	}
+
+	currentCommand := cmd[0]
+	command := exec.Command(currentCommand, cmd[1:]...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	err = command.Run()
+	if err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode()
+		}
+		log.Println(err)
+		return 1
+	}
+
 	return
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	const dirPath = "testdata/env"
+	env, err := ReadDir(dirPath)
+	require.NoError(t, err)
+
+	cmd := []string{"/bin/bash", "testdata/echo.sh", "arg1=1", "arg2=2"}
+	exitCode := RunCmd(cmd, env)
+	require.Equal(t, 0, exitCode)
+
+	cmd = []string{"/bin/bash", "testdata/invalid"}
+	exitCode = RunCmd(cmd, env)
+	require.Equal(t, 127, exitCode)
 }
