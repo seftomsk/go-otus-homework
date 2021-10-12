c569c6942501d78d3c34e15b88fcf160c5193591
Aleksei Verkhovtsev <seft@192.168.0.3>
2021-10-12 20:11:41 +0600

    HW9 is completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -19,7 +19,7 @@ type ValidationErrors []ValidationError
 func (v ValidationErrors) Error() string {
 	strBuilder := strings.Builder{}
 	for _, err := range v {
-		strBuilder.WriteString(fmt.Sprintf("Field: %s: %s\n", err.Field, err.Err))
+		strBuilder.WriteString("Field \"" + err.Field + "\": " + err.Err.Error() + "\n")
 	}
 	return strBuilder.String()
 }
@@ -29,20 +29,19 @@ type ParseError struct {
 	Err error
 }
 
-var (
-	ErrLength = errors.New("invalid length")
-	ErrMin    = errors.New("value less than min")
-	ErrMax    = errors.New("value more than max")
-	ErrIn     = errors.New("value is not included in the set")
-	ErrRegexp = errors.New("value is not match with regexp")
-)
-
 func (e ParseError) Error() string {
 	return e.Msg
 }
 
+var (
+	ErrLength = errors.New("invalid length")
+	ErrMin    = errors.New("number is less than min value")
+	ErrMax    = errors.New("number is more than max value")
+	ErrIn     = errors.New("value is not included in the set")
+	ErrRegexp = errors.New("value is not matched with regexp")
+)
+
 var supportedTypesOfStruct = make(map[reflect.Kind]struct{})
-var vErrors ValidationErrors
 
 func init() {
 	supportedTypesOfStruct[reflect.String] = struct{}{}
@@ -50,149 +49,281 @@ func init() {
 	supportedTypesOfStruct[reflect.Slice] = struct{}{}
 }
 
-func splitConstraint(list string, flag map[string]struct{}, field reflect.StructField) (string, string, error) {
+type validator struct {
+	errors             ValidationErrors
+	currentConstraints map[string]struct{}
+	currentStructField reflect.StructField
+}
+
+func (v *validator) splitConstraint(list string) (string, string, error) {
 	constraintWithValue := strings.Split(list, ":")
 	if len(constraintWithValue) != 2 {
 		return "", "", ParseError{
-			Msg: fmt.Sprintf("invalid constraint for field %q, expected [name:value(s)], but received %v", field.Name, constraintWithValue),
+			Msg: fmt.Sprintf(
+				"field %q - invalid constraint, expected [name:value(s)], but received %v",
+				v.currentStructField.Name,
+				constraintWithValue,
+			),
 		}
 	}
 	constraint := constraintWithValue[0]
-	constraintV := constraintWithValue[1]
 
-	if _, ok := flag[constraint]; ok {
+	if _, ok := v.currentConstraints[constraint]; ok {
 		return "", "", ParseError{
-			Msg: fmt.Sprintf("duplicate constraint %q for field %q", constraint, field.Name),
+			Msg: fmt.Sprintf(
+				"field %q - duplicate constraint %q",
+				v.currentStructField.Name,
+				constraint,
+			),
 		}
 	}
-	return constraint, constraintV, nil
+	return constraint, constraintWithValue[1], nil
 }
 
-func lenValidator(fv, constraint, constraintV string, field reflect.StructField) (bool, error) {
+func (v *validator) validateString(val string, constraintLists []string) error {
+	for _, list := range constraintLists {
+		constraint, constraintV, err := v.splitConstraint(
+			list,
+		)
+		if err != nil {
+			return err
+		}
+
+		v.currentConstraints[constraint] = struct{}{}
+		var ok bool
+
+		switch constraint {
+		case "len":
+			ok, err = v.strLen(val, constraint, constraintV) //nolint:staticcheck
+			if err != nil {
+				return err
+			}
+			if !ok {
+				break
+			}
+		case "in":
+			ok = v.strIn(val, constraintV) //nolint:staticcheck
+			if !ok {
+				break
+			}
+		case "regexp":
+			ok, err = v.strRegexp(val, constraint, constraintV) //nolint:staticcheck
+			if err != nil {
+				return err
+			}
+			if !ok {
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+func (v *validator) strLen(val, constraint, constraintV string) (bool, error) {
 	num, err := strconv.Atoi(constraintV)
 	if err != nil {
 		return false, ParseError{
-			Msg: fmt.Sprintf("expected a number value for %q constraint for field %q, but received %v", constraint, field.Name, constraintV),
+			Msg: fmt.Sprintf(
+				"field %q - expected a number value for %q constraint, but received %v",
+				v.currentStructField.Name,
+				constraint,
+				constraintV,
+			),
 			Err: err,
 		}
 	}
-	strLen := len(fv)
-	if strLen != num {
+
+	if len(val) != num {
 		vError := ValidationError{
-			Field: field.Name,
+			Field: v.currentStructField.Name,
 			Err:   fmt.Errorf("%w", ErrLength),
 		}
-		vErrors = append(vErrors, vError)
+		v.errors = append(v.errors, vError)
 		return false, nil
 	}
+
 	return true, nil
 }
 
-func inStringValidator(fv, constraintV string, field reflect.StructField) bool {
+func (v *validator) strIn(val, constraintV string) bool {
 	values := strings.Split(constraintV, ",")
 	in := false
-	for _, v := range values {
-		if v == fv {
+	for _, item := range values {
+		if item == val {
 			in = true
 		}
 	}
 	if !in {
 		vError := ValidationError{
-			Field: field.Name,
-			Err:   fmt.Errorf("values are not included in the set %v: %w", values, ErrIn),
+			Field: v.currentStructField.Name,
+			Err:   fmt.Errorf("%w", ErrIn),
 		}
-		vErrors = append(vErrors, vError)
+		v.errors = append(v.errors, vError)
 		return false
 	}
+
 	return true
 }
 
-func regexpValidator(fv, constraint, constraintV string, field reflect.StructField) (bool, error) {
+func (v *validator) strRegexp(val, constraint, constraintV string) (bool, error) {
 	reg, err := regexp.Compile(constraintV)
 	if err != nil {
 		return false, ParseError{
-			Msg: fmt.Sprintf("invalid regexp for %q constraint for field %q", constraint, field.Name),
+			Msg: fmt.Sprintf(
+				"field %q - invalid regexp for %q constraint",
+				v.currentStructField.Name,
+				constraint,
+			),
 			Err: err,
 		}
 	}
-	if !reg.MatchString(fv) {
+	if !reg.MatchString(val) {
 		vError := ValidationError{
-			Field: field.Name,
-			Err:   fmt.Errorf("values do not match to %v: %w", constraintV, ErrRegexp),
+			Field: v.currentStructField.Name,
+			Err:   fmt.Errorf("%w", ErrRegexp),
 		}
-		vErrors = append(vErrors, vError)
+		v.errors = append(v.errors, vError)
 		return false, nil
 	}
+
 	return true, nil
 }
 
-func minValidator(fv int64, constraint, constraintV string, field reflect.StructField) (bool, error) {
+func (v *validator) validateInt(val int64, constraintLists []string) error {
+	for _, list := range constraintLists {
+		constraint, constraintV, err := v.splitConstraint(
+			list,
+		)
+		if err != nil {
+			return err
+		}
+
+		v.currentConstraints[constraint] = struct{}{}
+		var ok bool
+
+		switch constraint {
+		case "min":
+			ok, err = v.intMin(val, constraint, constraintV) //nolint:staticcheck
+			if err != nil {
+				return err
+			}
+			if !ok {
+				break
+			}
+		case "max":
+			ok, err = v.intMax(val, constraint, constraintV) //nolint:staticcheck
+			if err != nil {
+				return err
+			}
+			if !ok {
+				break
+			}
+		case "in":
+			ok, err = v.intIn(val, constraint, constraintV) //nolint:staticcheck
+			if err != nil {
+				return err
+			}
+			if !ok {
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+func (v *validator) intMin(val int64, constraint, constraintV string) (bool, error) {
 	num, err := strconv.Atoi(constraintV)
 	if err != nil {
 		return false, ParseError{
-			Msg: fmt.Sprintf("expected a number value for %q constraint for field %q, but received %v", constraint, field.Name, constraintV),
+			Msg: fmt.Sprintf(
+				"field %q - expected a number value for %q constraint, but received %v",
+				v.currentStructField.Name,
+				constraint,
+				constraintV,
+			),
 			Err: err,
 		}
 	}
-	if fv < int64(num) {
+
+	if val < int64(num) {
 		vError := ValidationError{
-			Field: field.Name,
-			Err:   fmt.Errorf("numbers must be more than %v: %w", num, ErrMin),
+			Field: v.currentStructField.Name,
+			Err:   fmt.Errorf("%w", ErrMin),
 		}
-		vErrors = append(vErrors, vError)
+		v.errors = append(v.errors, vError)
 		return false, nil
 	}
+
 	return true, nil
 }
 
-func maxValidator(fv int64, constraint, constraintV string, field reflect.StructField) (bool, error) {
+func (v *validator) intMax(val int64, constraint, constraintV string) (bool, error) {
 	num, err := strconv.Atoi(constraintV)
 	if err != nil {
 		return false, ParseError{
-			Msg: fmt.Sprintf("expected a number value for %q constraint for field %q, but received %v", constraint, field.Name, constraintV),
+			Msg: fmt.Sprintf(
+				"field %q - expected a number value for %q constraint, but received %v",
+				v.currentStructField.Name,
+				constraint,
+				constraintV,
+			),
 			Err: err,
 		}
 	}
-	if fv > int64(num) {
+
+	if val > int64(num) {
 		vError := ValidationError{
-			Field: field.Name,
-			Err:   fmt.Errorf("numbers must be less than %v: %w", num, ErrMax),
+			Field: v.currentStructField.Name,
+			Err:   fmt.Errorf("%w", ErrMax),
 		}
-		vErrors = append(vErrors, vError)
+		v.errors = append(v.errors, vError)
 		return false, nil
 	}
+
 	return true, nil
 }
 
-func inIntValidator(fv int64, constraint, constraintV string, field reflect.StructField) (bool, error) {
+func (v *validator) intIn(val int64, constraint, constraintV string) (bool, error) {
 	values := strings.Split(constraintV, ",")
 	in := false
 	var num int
 	var err error
-	for _, v := range values {
-		num, err = strconv.Atoi(v)
+	for _, item := range values {
+		num, err = strconv.Atoi(item)
 		if err != nil {
 			return false, ParseError{
-				Msg: fmt.Sprintf("expected a number value for %q constraint for field %q, but received %v", constraint, field.Name, constraintV),
+				Msg: fmt.Sprintf(
+					"field %q - expected a number value for %q constraint, but received %v",
+					v.currentStructField.Name,
+					constraint,
+					constraintV,
+				),
 				Err: err,
 			}
 		}
-		if fv == int64(num) {
+		if val == int64(num) {
 			in = true
 		}
 	}
+
 	if !in {
 		vError := ValidationError{
-			Field: field.Name,
-			Err:   fmt.Errorf("numbers are not included in the set %v: %w", values, ErrIn),
+			Field: v.currentStructField.Name,
+			Err:   fmt.Errorf("%w", ErrIn),
 		}
-		vErrors = append(vErrors, vError)
+		v.errors = append(v.errors, vError)
 		return false, nil
 	}
+
 	return true, nil
 }
 
-func Validate(v interface{}) error {
+func Validate(v interface{}) error { //nolint:gocognit
+	worker := validator{
+		errors: make(ValidationErrors, 0),
+	}
 	rValue := reflect.ValueOf(v)
 	if rValue.Kind() != reflect.Struct {
 		return ParseError{
@@ -222,148 +353,41 @@ func Validate(v interface{}) error {
 			continue
 		}
 
-		constraintFlag := make(map[string]struct{})
+		worker.currentConstraints = make(map[string]struct{})
+		worker.currentStructField = field
 		constraintLists := strings.Split(validate, "|")
 
 		switch fieldValue.Kind() {
 		case reflect.String:
-			fv := fieldValue.String()
-			for _, list := range constraintLists {
-				constraint, constraintV, err := splitConstraint(list, constraintFlag, field)
-				if err != nil {
-					return err
-				}
-				constraintFlag[constraint] = struct{}{}
-				switch constraint {
-				case "len":
-					_, err = lenValidator(fv, constraint, constraintV, field)
-					if err != nil {
-						return err
-					}
-				case "in":
-					inStringValidator(fv, constraintV, field)
-				case "regexp":
-					_, err = regexpValidator(fv, constraint, constraintV, field)
-					if err != nil {
-						return err
-					}
-				}
+			if err := worker.validateString(fieldValue.String(), constraintLists); err != nil {
+				return err
 			}
 		case reflect.Int:
-			fv := fieldValue.Int()
-			for _, list := range constraintLists {
-				constraint, constraintV, err := splitConstraint(list, constraintFlag, field)
-				if err != nil {
-					return err
-				}
-				constraintFlag[constraint] = struct{}{}
-				switch constraint {
-				case "min":
-					_, err = minValidator(fv, constraint, constraintV, field)
-					if err != nil {
-						return err
-					}
-				case "max":
-					_, err = maxValidator(fv, constraint, constraintV, field)
-					if err != nil {
-						return err
-					}
-				case "in":
-					_, err = inIntValidator(fv, constraint, constraintV, field)
-					if err != nil {
-						return err
-					}
-				}
+			if err := worker.validateInt(fieldValue.Int(), constraintLists); err != nil {
+				return err
 			}
 		case reflect.Slice:
 			switch fieldValue.Interface().(type) {
 			case []int:
-				sliceFv := fieldValue.Interface().([]int)
-				for _, list := range constraintLists {
-					constraint, constraintV, err := splitConstraint(list, constraintFlag, field)
-					if err != nil {
+				sliceValues := fieldValue.Interface().([]int)
+				for _, val := range sliceValues {
+					if err := worker.validateInt(int64(val), constraintLists); err != nil {
 						return err
-					}
-					constraintFlag[constraint] = struct{}{}
-					switch constraint {
-					case "min":
-						for _, fv := range sliceFv {
-							ok, err = minValidator(int64(fv), constraint, constraintV, field)
-							if err != nil {
-								return err
-							}
-							if !ok {
-								break
-							}
-						}
-					case "max":
-						for _, fv := range sliceFv {
-							ok, err = maxValidator(int64(fv), constraint, constraintV, field)
-							if err != nil {
-								return err
-							}
-							if !ok {
-								break
-							}
-						}
-					case "in":
-						for _, fv := range sliceFv {
-							ok, err = inIntValidator(int64(fv), constraint, constraintV, field)
-							if err != nil {
-								return err
-							}
-							if !ok {
-								break
-							}
-						}
 					}
 				}
 			case []string:
-				sliceFv := fieldValue.Interface().([]string)
-				for _, list := range constraintLists {
-					constraint, constraintV, err := splitConstraint(list, constraintFlag, field)
-					if err != nil {
+				sliceValues := fieldValue.Interface().([]string)
+				for _, val := range sliceValues {
+					if err := worker.validateString(val, constraintLists); err != nil {
 						return err
-					}
-					constraintFlag[constraint] = struct{}{}
-					switch constraint {
-					case "len":
-						for _, fv := range sliceFv {
-							ok, err = lenValidator(fv, constraint, constraintV, field)
-							if err != nil {
-								return err
-							}
-							if !ok {
-								break
-							}
-						}
-					case "in":
-						for _, fv := range sliceFv {
-							ok = inStringValidator(fv, constraintV, field)
-							if err != nil {
-								return err
-							}
-							if !ok {
-								break
-							}
-						}
-					case "regexp":
-						for _, fv := range sliceFv {
-							ok, err = regexpValidator(fv, constraint, constraintV, field)
-							if err != nil {
-								return err
-							}
-							if !ok {
-								break
-							}
-						}
 					}
 				}
 			}
 		}
 	}
-	if len(vErrors) != 0 {
-		return vErrors
+	if len(worker.errors) != 0 {
+		return worker.errors
 	}
+
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,10 +2,10 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -13,13 +13,14 @@ type UserRole string
 // Test the function on different structures and other types.
 type (
 	User struct {
-		ID     string `json:"id" validate:"len:36"`
-		Name   string
-		Age    int      `validate:"min:18|max:50"`
-		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole `validate:"in:admin,stuff"`
-		Phones []string `validate:"len:11"`
-		meta   json.RawMessage
+		ID        string `json:"id" validate:"len:36"`
+		Name      string
+		Age       int      `validate:"min:18|max:50"`
+		Email     string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role      UserRole `validate:"in:admin,stuff"`
+		Phones    []string `validate:"len:11"`
+		CityCodes []int    `validate:"min:2|max:3"`
+		meta      json.RawMessage
 	}
 
 	App struct {
@@ -36,6 +37,31 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	Duplicate struct {
+		Name string `validate:"len:2|len:3"`
+	}
+
+	InvalidConstraint struct {
+		Name string `validate:"len:2:4"`
+	}
+
+	InvalidConstraintArguments struct {
+		Name string `validate:"len:sdf"`
+	}
+
+	InvalidConstraintRegexp struct {
+		Name string `validate:"regexp:\\\\\\\\\\"`
+	}
+
+	PrivateFields struct {
+		name string `validate:"len:5"`
+		age  int    `validate:"min:18|max:50"`
+	}
+
+	NotSupportedFields struct {
+		Name struct{} `validate:"len:5"`
+	}
 )
 
 func TestValidate(t *testing.T) {
@@ -43,9 +69,47 @@ func TestValidate(t *testing.T) {
 		in          interface{}
 		expectedErr error
 	}{
-		//{App{Version: "1234"}, ErrLength},
-		//{App{Version: "12345"}, nil},
+		{App{Version: "1234"}, ValidationErrors{ValidationError{"Version", ErrLength}}},
+		{App{Version: "12345"}, nil},
 		{App{Version: "123456"}, ValidationErrors{ValidationError{Field: "Version", Err: ErrLength}}},
+
+		{Response{Code: 199, Body: ""}, ValidationErrors{ValidationError{Field: "Code", Err: ErrIn}}},
+		{Response{Code: 200, Body: ""}, nil},
+		{Response{Code: 201, Body: ""}, ValidationErrors{ValidationError{Field: "Code", Err: ErrIn}}},
+
+		{Response{Code: 403, Body: ""}, ValidationErrors{ValidationError{Field: "Code", Err: ErrIn}}},
+		{Response{Code: 404, Body: ""}, nil},
+		{Response{Code: 405, Body: ""}, ValidationErrors{ValidationError{Field: "Code", Err: ErrIn}}},
+
+		{Response{Code: 499, Body: ""}, ValidationErrors{ValidationError{Field: "Code", Err: ErrIn}}},
+		{Response{Code: 500, Body: ""}, nil},
+		{Response{Code: 501, Body: ""}, ValidationErrors{ValidationError{Field: "Code", Err: ErrIn}}},
+
+		{User{
+			ID:        "",
+			Name:      "",
+			Age:       0,
+			Email:     "",
+			Role:      "",
+			Phones:    []string{""},
+			CityCodes: []int{0},
+			meta:      json.RawMessage(""),
+		}, ValidationErrors([]ValidationError{
+			{Field: "ID", Err: ErrLength},
+			{Field: "Age", Err: ErrMin},
+			{Field: "Email", Err: ErrRegexp},
+			{Field: "Role", Err: ErrIn},
+			{Field: "Phones", Err: ErrLength},
+			{Field: "CityCodes", Err: ErrMin},
+		})},
+		{User{
+			ID:     "123456789123456789123456789123456789",
+			Name:   "",
+			Age:    19,
+			Email:  "test@test.com",
+			Role:   "admin",
+			Phones: []string{"12345678912"},
+		}, nil},
 	}
 
 	for i, tt := range tests {
@@ -54,19 +118,50 @@ func TestValidate(t *testing.T) {
 			t.Parallel()
 
 			err := Validate(tt.in)
-			//if tt.expectedErr == nil {
-			//	require.NoError(t, err)
-			//} else {
-			//	require.EqualError(t, err, tt.expectedErr.Error())
-			//}
 
-			var vErrors ValidationErrors
-			require.ErrorAs(t, err, &vErrors)
-			if errors.As(err, &vErrors) {
-				for _, r := range vErrors {
-					require.ErrorIs(t, r.Err, tt.expectedErr)
-				}
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+			} else {
+				require.ErrorAs(t, err, &ValidationErrors{})
+				require.EqualError(t, err, tt.expectedErr.Error())
 			}
 		})
 	}
+}
+
+func TestDuplicateConstraint(t *testing.T) {
+	err := Validate(Duplicate{Name: ""})
+	require.ErrorAs(t, err, &ParseError{})
+}
+
+func TestNotAStruct(t *testing.T) {
+	err := Validate(2)
+	require.ErrorAs(t, err, &ParseError{})
+}
+
+func TestInvalidConstraintSignature(t *testing.T) {
+	err := Validate(InvalidConstraint{Name: ""})
+	require.ErrorAs(t, err, &ParseError{})
+}
+
+func TestInvalidConstraintArguments(t *testing.T) {
+	err := Validate(InvalidConstraintArguments{Name: ""})
+	require.ErrorAs(t, err, &ParseError{})
+}
+
+func TestInvalidConstraintRegexp(t *testing.T) {
+	err := Validate(InvalidConstraintRegexp{Name: ""})
+	require.ErrorAs(t, err, &ParseError{})
+}
+
+func TestPrivateFields(t *testing.T) {
+	err := Validate(PrivateFields{name: "", age: 30})
+	require.NoError(t, err)
+}
+
+func TestNotSupportedFields(t *testing.T) {
+	err := Validate(NotSupportedFields{Name: struct{}{}})
+	require.NoError(t, err)
+	err = Validate(Token{})
+	require.NoError(t, err)
 }
